ed25438dc166572eb2452159e7d20e06349f1c22
Tomas New Bauer <tomas+staging@quix.io>
2025-08-19 07:52:40 +0000

    Copied files from Data normalisation

---
+++ data-transposition/main.py
@@ -0,0 +1,65 @@
+# import the Quix Streams modules for interacting with Kafka.
+# For general info, see https://quix.io/docs/quix-streams/introduction.html
+from quixstreams import Application
+from quixstreams.dataframe.windows.aggregations import Last, Count, Mean
+from datetime import datetime
+import os
+
+# for local dev, load env vars from a .env file
+# from dotenv import load_dotenv
+# load_dotenv()
+
+
+def main():
+
+    # Setup necessary objects
+    app = Application(
+        consumer_group="data-norm-v1.1",
+        auto_create_topics=True,
+        auto_offset_reset="earliest"
+    )
+    input_topic = app.topic(name=os.environ["input"], key_deserializer="str")
+    output_topic = app.topic(name=os.environ["output"])
+    sdf = app.dataframe(topic=input_topic)
+
+    sdf = sdf.apply(lambda value, key, timestamp, h: {
+        "timestamp": timestamp,
+        "machine": key.split("/")[0],
+        "sensor_id": key.split("/")[1],
+        "value": value
+    }, metadata=True)
+
+    sdf = sdf.apply(lambda row: {
+        "timestamp": row["timestamp"],
+        "machine": row["machine"],
+
+        row["sensor_id"]: row["value"]
+    })
+
+    sdf = sdf.group_by("machine")
+
+    sdf = sdf.hopping_window(1000, 100, 1000).agg(
+        FAN_SPEED=Last("FAN_SPEED"),
+        BED_TEMPERATURE=Last("BED_TEMPERATURE"),
+        PRINT_SPEED=Last("PRINT_SPEED")
+    ).final()
+
+    sdf["timestamp"] = sdf["start"]
+
+    sdf = sdf.drop(["start", "end"])
+
+    sdf = sdf.fill("FAN_SPEED", "BED_TEMPERATURE", "PRINT_SPEED", "AUX_TEMP")
+
+    # Do StreamingDataFrame operations/transformations here
+    sdf = sdf.print_table(metadata=False)
+
+    # Finish off by writing to the final result to the output topic
+    sdf.to_topic(output_topic)
+
+    # With our pipeline defined, now run the Application
+    app.run()
+
+
+# It is recommended to execute Applications under a conditional main
+if __name__ == "__main__":
+    main()
