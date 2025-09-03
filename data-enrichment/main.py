cc94f14a83a771beb8d1f306dbca584e7f45d70a
Tomas New Bauer <tomas+staging@quix.io>
2025-09-03 15:07:42 +0000

    Edited 'main.py'

--- data-enrichment/main.py
+++ data-enrichment/main.py
@@ -1,6 +1,7 @@
 # import the Quix Streams modules for interacting with Kafka.
 # For general info, see https://quix.io/docs/quix-streams/introduction.html
 from quixstreams import Application
+from quixstreams.dataframe.joins.lookups import 
 
 import os
 
@@ -19,11 +20,42 @@ def main():
     )
     input_topic = app.topic(name=os.environ["input"])
     output_topic = app.topic(name=os.environ["output"])
+    config_topic = app.topic(name=os.environ["CONFIG_TOPIC"])
+
     sdf = app.dataframe(topic=input_topic)
 
-    # Do StreamingDataFrame operations/transformations here
-    sdf = sdf.apply(lambda row: row).filter(lambda row: True)
-    sdf = sdf.print(metadata=True)
+    enricher = QuixConfigurationService(
+        topic=config_topic,
+        app_config=app.config,
+    )
+
+    # Enrich data using defined configs (lookup_join)
+    sdf = sdf.join_lookup(
+        lookup=enricher,
+        fields={
+            "editor_name": Field(
+                type="printer-config",
+                default=None,
+                jsonpath="editor_name"
+            ),
+            "field_scalar": Field(
+                type="printer-config",
+                default=1.0,
+                jsonpath="field_scalar"
+            ),
+            "mapping": Field(
+                type="printer-config",
+                default={},
+                jsonpath="mapping"
+            ),
+        }
+    ).apply(config_apply)
+
+    # Finish off by writing to the final result to the output topic
+    sdf.to_topic(output_topic, key=lambda row: row["machine"])
+
+    # With our pipeline defined, now run the Application
+    app.run()
 
     # Finish off by writing to the final result to the output topic
     sdf.to_topic(output_topic)
